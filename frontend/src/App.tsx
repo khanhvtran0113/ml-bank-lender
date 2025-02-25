1bed14eb53d2836334f31570eb9a901877faebfc
Khanh Tran <vktran@stanford.edu>
2025-02-25 00:38:46 -0500

    Add Tremor demo to FE

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
 import axios from 'axios';
 import React from 'react';
+import { DonutChart } from './components/pieChart';
+
+
+const chartdata = [  {    name: "SolarCells",    amount: 4890,  },  {    name: "Glass",    amount: 2103,  },  {    name: "JunctionBox",    amount: 2050,  },  {    name: "Adhesive",    amount: 1300,  },  {    name: "BackSheet",    amount: 1100,  },  {    name: "Frame",    amount: 700,  },  {    name: "Encapsulant",    amount: 200,  },]
 
 export function App() {
-  const [message, setMessage] = React.useState<string>('');
+  const [, setMessage] = React.useState<string>('');
 
 	React.useEffect(() => {
 		axios.get('http://127.0.0.1:5000/')
@@ -12,7 +16,7 @@ export function App() {
   
   return (
     <div>
-      {message}
+      <DonutChart    className="mx-auto"    data={chartdata}    category="name"    value="amount"    showLabel={true}    valueFormatter={(number: number) =>      `$${Intl.NumberFormat("us").format(number).toString()}`    }  />
     </div>
   );
 }
